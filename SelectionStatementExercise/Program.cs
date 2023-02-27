5dc8d4ba4ccffef1855201a0cd67fd7bb7e88025
fred47 <Frdrck.hart@gmail.com>
2023-02-27 05:39:37 -0500

    Finished Selection Statements Exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Reflection.Metadata;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //Number Guessing Game
+            
+
+            var r = new Random();
+            var favNumber = r.Next(1, 1001);
+            
+           
+            Console.WriteLine("Guess my favorite number from 1-1000?");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if(userInput < favNumber)
+            {
+                Console.WriteLine($"Number {userInput} too low");
+            }
+            if(userInput > favNumber)
+            {
+                Console.WriteLine($"Number {userInput} too high");
+            }
+            if(userInput == favNumber)
+            {
+                Console.WriteLine("You Guessed my number!");
+            }
+
+            
+
         }
     }
 }
